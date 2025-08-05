070bc9f4d9eddeda959ea8aca6462aeacbaefe87
adadachao <adadachao@gmail.com>
2025-08-05 16:16:27 +0800

    config up

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/faucet/:path*',
-        destination: 'http://15.206.227.101:38070/api/faucet/:path*',
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/faucet/:path*',
+  //       destination: 'http://15.206.227.101:38070/api/faucet/:path*',
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
